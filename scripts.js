741cf27537a093dbdc9dc37c46e35c214da67ec6
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-07-08 16:49:08 -0300

    Commit da conclusão do projeto

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+    const body = document.body;
+
+    // if (body.classList.contains("light")) {
+    //     body.classList.remove("light")
+    // } else {
+    //     body.classList.add("light")
+    // }
+
+    body.classList.toggle("light");
+}
